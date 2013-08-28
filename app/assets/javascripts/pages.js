a6c8c44dd5db9e3dee2740f0c4f4af54aec14e3b
Jacob Burenstam <burenstam@gmail.com>
2013-08-28 23:20:19 +0200

    added .clone() method to image element

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,6 @@
 $(document).on('click', '.blog-post img, .gallery-box img, .static-box img', function(e) {
   e.preventDefault();
   var image = $(this);
-  $('#image-target').html(image);
+  $('#image-target').html(image.clone());
   $('#image-modal').modal('show');
 });
