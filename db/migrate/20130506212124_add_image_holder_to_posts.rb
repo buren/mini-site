aa56e49db05df6f484d4a0974d5443c8193d0a55
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-03 22:56:02 +0800

    Delete db directory

--- db/migrate/20130506212124_add_image_holder_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddImageHolderToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :image_holder, :text
-  end
-end
--- db/migrate/20140207152248_add_gallery_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddGalleryToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :gallery, :text
-  end
-end
