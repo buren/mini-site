798904bdb8483fd50cf35e7a7f04bd3f50a4343a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-03 22:55:38 +0800

    Delete config directory

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BlogApplication::Application.config.secret_token = '3616fde8146ea26289445912695e9b32e7c32ce5f650ea8882ac7610e99c105cd2cfcf32a8cd32f11bb0860aec117d3ceed56142a08bf45dfa0efa70baad629b'
