aea61f46f9196f5f2880c764d80790356619099e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-03 22:55:24 +0800

    Delete app directory

--- app/assets/javascripts/mercury_page.js
+++
@@ -1,5 +0,0 @@
-$(window).on('mercury:ready', function() {
-  Mercury.on('saved', function() {
-		window.location = window.location.href.replace(/\/editor\//i, '/').replace('editing_post_id=', '');
-  });
-});
